c1f39db4d30a2473783157485c47304a0aba0da4
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-09 11:27:53 +0100

    feat: moved tycoon out

--- frontend/app/game-waiting/page.tsx
+++
@@ -1,9 +0,0 @@
-import GameWaiting from "@/components/settings/game-waiting";
-
-export default function GameWaitingPage() {
-    return (
-        <main className="w-full overflow-x-hidden">
-                <GameWaiting />
-        </main>
-    );
-}
--- frontend/app/play-ai/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlayWithAISettings from "@/components/settings/game-ai";
-
-export default function GameSettingsPage() {
-    return (
-        <main className="w-full overflow-x-hidden">
-            <PlayWithAISettings />
-        </main>
-    );
-}
